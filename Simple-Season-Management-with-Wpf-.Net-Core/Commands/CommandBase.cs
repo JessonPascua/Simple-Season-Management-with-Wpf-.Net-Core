643d396a828bc0ea5a668f38820db6b6b0bb9775
JessonWork <jessonpascua.work@gmail.com>
2023-06-23 21:05:13 +0800

    update

--- Simple-Season-Management-with-Wpf-.Net-Core/Commands/CommandBase.cs
+++ Simple-Season-Management-with-Wpf-.Net-Core/Commands/CommandBase.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Simple_Season_Management_with_Wpf_.Net_Core.Commands
 {
-    internal class CommandBase
+    public abstract class CommandBase : ICommand
     {
+        public event EventHandler? CanExecuteChanged;
+
+        public virtual bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public abstract void Execute(object? parameter);
+
+        protected virtual void OnCanExecutedChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
     }
 }
--- Simple-Season-Management-with-Wpf-.Net-Core/ViewModel/ViewModel.cs
+++ Simple-Season-Management-with-Wpf-.Net-Core/ViewModel/ViewModel.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Simple_Season_Management_with_Wpf_.Net_Core.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Simple_Season_Management_with_Wpf_.Net_Core.ViewModel
 {
-    internal class ViewModel
+    public class ViewModel : ViewModelBase
     {
+        public ICommand SignInCommand { get; set; }
+
+        public ViewModel()
+        {
+            SignInCommand = new SignInCommand();
+        }
     }
 }
