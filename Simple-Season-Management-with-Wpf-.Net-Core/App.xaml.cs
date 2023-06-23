da70398f76a415870165f25e01fe6c1355b47e9a
JessonWork <jessonpascua.work@gmail.com>
2023-06-23 19:16:07 +0800

    Add project files.

---
+++ Simple-Season-Management-with-Wpf-.Net-Core/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Simple_Season_Management_with_Wpf_.Net_Core
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
