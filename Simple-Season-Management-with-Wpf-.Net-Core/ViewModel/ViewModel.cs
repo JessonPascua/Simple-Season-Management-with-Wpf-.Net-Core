1e59797fb765f62064dc418829a3cfe824943b26
JessonWork <jessonpascua.work@gmail.com>
2023-06-24 13:55:17 +0800

    update

--- Simple-Season-Management-with-Wpf-.Net-Core/ViewModel/ViewModel.cs
+++ Simple-Season-Management-with-Wpf-.Net-Core/ViewModel/ViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -10,6 +11,28 @@ namespace Simple_Season_Management_with_Wpf_.Net_Core.ViewModel
 {
     public class ViewModel : ViewModelBase
     {
+        private string? _username;
+        public string? Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        private SecureString? _password;
+        public SecureString? Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
         public ICommand SignInCommand { get; set; }
         public ICommand LogInCommand { get; set; }
 
